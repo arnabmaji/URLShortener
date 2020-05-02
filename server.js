24141f432f5beede8f8a34a609432db499b00848
Arnab Maji <arnabmaji1999@gmail.com>
2020-05-02 14:16:15 +0530

    setup mongodb

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const { validate } = require('./models/UrlMapper');
+const { UrlMapper, validate } = require('./models/UrlMapper');
 
 const app = express();
+
+// connect to mongodb
+mongoose
+    .connect('mongodb://localhost/url-shortener', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log('Connected to MongoDb'))
+    .catch(err => {
+        console.log('Failed to connect to MongoDB');
+        console.log('ERROR: ', err.message);
+        console.log('Terminating Server...');
+        process.exit(1);
+    });
 
 // middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // routes
+// add home route
 app.get('/', (req, res) => {
     res.send('Url Shortener');
 });
