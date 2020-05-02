45989fda134e0c6a22273a1284d95a751b3ca6a2
Arnab Maji <arnabmaji1999@gmail.com>
2020-05-02 12:15:07 +0530

    setup express

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('URL Shortener');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+// middleware
+app.use(bodyParser.urlencoded({extended: true}));
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Url Shortener');
+});
+
+// start the server on port
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}...`));
